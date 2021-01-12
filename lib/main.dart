e8e74a5fdbfd56adffa2101a83c8e85f96eb94d3
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2021-01-12 14:49:53 +0700

    COMMIT : Stack Promotion DONE

--- lib/main.dart
+++ lib/main.dart
@@ -29,6 +29,154 @@ class MyHomePage extends StatelessWidget {
       body: Container(
         height: screenHeight,
         width: screenWidth,
+        child: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(icon: Icon(Icons.menu), onPressed: () {}),
+                      Text("Tumbas",
+                          style:
+                              TextStyle(fontSize: 50, fontFamily: "Samantha")),
+                      IconButton(icon: Icon(Icons.person), onPressed: () {}),
+                    ],
+                  ),
+                ),
+                BannerWidgetArea()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+var bannerItems = ["Burger", "Cheese Chilly", "Noodles", "Pizza"];
+var bannerDescriptions = [
+  "Choose any kinds of burger style",
+  "Dare to taste of our hell chilly souce?",
+  "Noodles everytime!",
+  "Authentic Pizza in this Town"
+];
+var bannerImage = [
+  "images/burger.jpg",
+  "images/cheesechilly.jpg",
+  "images/noodles.jpg",
+  "images/pizza.jpg"
+];
+
+class BannerWidgetArea extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //var screenHeight = MediaQuery.of(context).size.height;
+    var screenWidth = MediaQuery.of(context).size.width;
+    PageController controller = PageController(
+      viewportFraction: 0.8,
+      initialPage: 1,
+    );
+
+    List<Widget> banners = new List<Widget>();
+
+    for (int x = 0; x < bannerItems.length; x++) {
+      var bannerView = Padding(
+        padding: EdgeInsets.all(10.0),
+        child: Container(
+          child: Stack(
+            fit: StackFit.expand,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black54,
+                      offset: Offset(2.0, 2.0),
+                      blurRadius: 5.0,
+                      spreadRadius: 1.0,
+                    )
+                  ],
+                ),
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                child: Image.asset(
+                  bannerImage[x],
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                  gradient: LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [Colors.transparent, Colors.black],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      bannerItems[x],
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      bannerDescriptions[x],
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 0,
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+                  decoration: BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8),
+                        bottomRight: Radius.circular(8),
+                      ) // green shaped
+                      ),
+                  child: Text(
+                    "PROMO",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+      banners.add(bannerView);
+    }
+
+    return Container(
+      width: screenWidth,
+      height: screenWidth * 9 / 16,
+      child: PageView(
+        controller: controller,
+        scrollDirection: Axis.horizontal,
+        children: banners,
       ),
     );
   }
